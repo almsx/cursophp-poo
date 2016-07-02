04641b89b5c81dcaadd2088608abe2195859f81e
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-07-02 12:37:46 -0500

    Login basico

--- Dia3/login.php
+++ Dia3/login.php
@@ -1,21 +1,43 @@
 <?php 
 
-$usuario = $_POST['userBD'];
-$contras = $_POST['passBD'];
 
 
-include("../utils/cnxdia3.php");
+if (empty($_POST["userBD"]) && empty($_POST["passBD"])){
+    echo "Vienen vacios o no iniciaste la sesión";
+    echo "<br/>";
+    echo "<a href=login.html>  Iniciar Sesion</a>";
+    
+} else {
 
-$view_users_query="SELECT idAutor, nombre, apellido FROM Autores WHERE usuario ='".$usuario."' AND password='".$contras."';"; 
+	//header("Location: panel.php");
+	//echo "Si iniciaste sesion";
 
-$run=mysqli_query($dbcon,$view_users_query);  
-  
-while($row=mysqli_fetch_array($run)) {  
-	$nombre=$row[1];  
-    $apellido=$row[2];  
-            
-  	echo $nombre.' '.$apellido;  
+	$usuario = $_POST['userBD'];
+	$contras = $_POST['passBD'];
+
+
+	include("../utils/cnxdia3.php");
+
+	$view_users_query="SELECT idAutor, nombre, apellido FROM Autores WHERE usuario ='".$usuario."' AND password='".$contras."';"; 
+
+	$run=mysqli_query($dbcon,$view_users_query);  
+
+	$filas = mysqli_num_rows($run);
+
+	//echo 'Hay '.$filas.' resultados';
+
+	if($filas !=0){
+		
+		header("Location: panel.php");
+
+	} else {
+		echo "Usuario no existe, favor de verificar";
+		echo "<br/>";
+    	echo "<a href=login.html>  Iniciar Sesion</a>";
+
+	}
 }
+
 
 
 
