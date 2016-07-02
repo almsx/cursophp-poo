21fc445c4b66652316bceafd6fb96588bf49bc2e
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-07-02 13:58:57 -0500

    Añadir Entrada y Categoria

---
+++ Dia3/blog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Blog</h1>
+	<a href="nuevaEntrada.php">Nueva Entrada</a>
+	<h1>Categorias</h1>
+	<a href="nuevaCategoria.php">Nueva Categoria</a>
+</body>
+</html>
