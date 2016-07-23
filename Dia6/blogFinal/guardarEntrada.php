a69bbf7d572e81423c9851899f88d48bc4a1ab61
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-07-23 12:04:44 -0500

    Insertar Noticia

--- Dia6/blogFinal/guardarEntrada.php
+++ Dia6/blogFinal/guardarEntrada.php
@@ -1,7 +1,37 @@
 <?php 
 
 include("seguridad.php"); 
+include("../../utils/cnxdia3.php");
+//print_r($_POST);
 
-print_r($_POST);
+if (empty($_POST["pTituloNoticia"]) && empty($_POST["categoriaPublicacion"])  && empty($_POST["pNoticia"])  ){
+    
+    echo "No puedes insertar Noticias vacias";
+    echo "<br/>";
+    echo "<a href=blog.php>  Ir a Administrador del Blog</a>";
+    
+} else {
+
+	//$usuario = $_POST['pUsuario'];
+	//$contras = $_POST['pContra'];
+
+	$tituloNoticia = $_POST['pTituloNoticia'];
+	$categoriaPublicacion = $_POST['categoriaPublicacion'];
+	$noticiaCompleta = $_POST['pNoticia'];
+	$idAutor = $_SESSION['idAutor'];
+
+	$queryInsert = "INSERT INTO Noticias (tituloNoticia,contenidoNoticia,idCategoria,idAutor) VALUES ('$tituloNoticia','$noticiaCompleta','$categoriaPublicacion','$idAutor');";
+	
+
+	if(mysqli_query($dbcon, $queryInsert)){
+    	
+    	echo "Inserción correcta";
+    	echo "<br/>";
+    	echo "<a href='blog.php'>Ir al Administrador del Blog</a>";
+	} else{
+    	echo "Error en la consulta $queryInsert";
+	}
+
+}
 
 ?>
