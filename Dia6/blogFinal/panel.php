e3e529b760414b69616fb19b3ed64ae7b1ccc737
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-07-23 11:06:25 -0500

    Panel de Control

--- Dia6/blogFinal/panel.php
+++ Dia6/blogFinal/panel.php
@@ -1,9 +1,20 @@
-<?php 
-
-include("seguridad.php"); 
-//session_start();
-echo "Bienvenido al Administrador del Blog, ".$_SESSION['nombreApp']. " <a href=logout.php>Cerrar Sesión</a>";
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	include("seguridad.php"); 
+	//session_start();
+	echo "Bienvenido al Administrador del Blog, ".$_SESSION['nombreApp']. " <a href=logout.php>Cerrar Sesión</a>";
+	?>
+<ul>
+		<li><a href="albums.php">Albums</a></li>
+		<li><a href="autores.php">Autores</a></li>
+		<li><a href="blog.php">Blog</a></li>
+		<li><a href="encuestas.php">Encuestas</a></li>
+	</ul>
+</body>
+</html>
